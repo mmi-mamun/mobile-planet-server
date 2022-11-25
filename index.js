607a8703a0e0c33b864c5eba7228e791e35560fe
mmi-mamun <muzahidulislam735@gmail.com>
2022-11-25 10:15:05 +0600

    Load all phones from mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -17,4 +17,25 @@ app.listen(port, () => {
 })
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijax3zt.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const phoneCollection = client.db('mobilePlanet').collection('allPhones');
+
+        app.get('/allPhones', async (req, res) => {
+            const query = {};
+            const phones = await phoneCollection.find(query).toArray();
+            res.send(phones);
+        })
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.log);
 
