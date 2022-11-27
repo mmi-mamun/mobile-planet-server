dbec0d01041508d79d705953feeb671a51e4224e
mmi-mamun <muzahidulislam735@gmail.com>
2022-11-27 16:47:30 +0600

    use jwt token for every registered user is done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -23,10 +24,31 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//middleware function
+function verifyJWT(req, res, next) {
+    console.log('token inside verifyJWT', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('Unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         const phoneCollection = client.db('mobilePlanet').collection('allPhones');
         const bookingCollection = client.db('mobilePlanet').collection('bookPhones');
+        const userCollection = client.db('mobilePlanet').collection('users');
 
         app.get('/allPhones', async (req, res) => {
             const query = {};
@@ -58,11 +80,38 @@ async function run() {
         })
 
         // get booking data for each buyer
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = { Email: email };
+            // console.log('token', req.headers.authorization);
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
+        })
+
+        // store user data
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // jwt
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            // console.log('token', req.headers.authorization)
+            const user = await userCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token })
+            }
+            // console.log(user);
+            res.status(403).send({ accessToken: '' })
         })
     }
 
